df634e8d6e9645641d848b070c9675444d0ce802
webdev-rahul <fly2rahulm@outlook.com>
2021-09-01 23:55:49 +0530

    newc-onfig

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sample project to deploy on Google Cloud Platform.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
